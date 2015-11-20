ee8150e718f37d761d01763efe0753e529413813
Ashok Borra <coded9@users.noreply.github.com>
2015-11-20 16:03:54 +0530

    Response in html
    Colors module is also used

--- simple server.js
+++ simple server.js
@@ -5,3 +5,12 @@ http.createServer(function(req,res){
 	res.end('Hello World\n');
 }).listen(1337,'127.0.0.1');
 console.log('Server running at localhost:1337');
+
+/*Version 2 */
+var http = require('http');
+var colors = require('colors');
+http.createServer(function(req,res){
+	res.writeHead(200,{'Content-Type':'text/html'});
+	res.end("<h1 style='color:blue'>Hello World</h1>\n");
+}).listen(1337,'127.0.0.1');
+console.log('Server running at localhost:1337'.green);
