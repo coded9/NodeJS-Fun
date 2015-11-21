f349ef1f6fa62a24687beef8b49006794dc896eb
coded9 <ashok.rex.2009@gmail.com>
2015-11-21 08:39:50 +0530

    Adding math-module

--- math-module/index.js
+++ math-module/index.js
@@ -1,1 +1,27 @@
+exports.add = add;
+exports.subtract = subtract;
+exports.multiply = multiply;
+exports.divide = divide;
+exports.factorial = factorial;
+exports.now = Date.now();
 
+function add(number1,number2){
+	return parseInt(number1,10)+parseInt(number2,10);
+}
+function subtract(number1,number2){
+	return parseInt(number1,10)-parseInt(number2,10);
+}
+function multiply(number1,number2){
+	return parseInt(number1,10)*parseInt(number2,10);
+}
+function divide(number1,number2){
+	return parseInt(number1,10)/parseInt(number2,10);
+}
+function factorial(number){
+	if(number===0){
+		return 1;
+	}
+	else{
+		return number*factorial(number-1);
+	}
+}
